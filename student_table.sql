f4e63779021b77dded328c3d877e6815c03153df
jyotiprakash20 <jyotiprakashpadhi30@gmail.com>
2025-09-23 22:50:50 +0530

    Initial commit: Added beginner level

---
+++ student_table.sql
@@ -0,0 +1,55 @@
+-- use jyoti
+
+-- CREATE table student_table (
+-- 	id int auto_increment primary key,
+-- 	name varchar(100) not null default 'not null',
+-- 	age int,
+-- 	grade varchar(20),
+-- 	marks int,
+-- 	city varchar(50),
+-- 	email varchar(100) unique
+-- );
+
+-- describe student_table;
+
+-- insert into student_table (id, name, age, grade, marks, city,email)
+-- value(001, 'Amit', 19, 'A', 95, 'Bhadrak', 'amit19@gmail.com');
+
+-- insert into student_table (id, name, age, grade, marks, city,email)
+-- value(002, 'Roshan', 20, 'D', 65, 'Bhopal', 'roshan20@gmail.com');
+
+-- insert into student_table (id, name, age, grade, marks, city,email)
+-- value(003, 'Jyoti', 21, 'B', 78, 'Bhadrak', 'jyotiprakashpadhi30@gmail.com');
+
+-- insert into student_table (id, name, age, grade, marks, city,email)
+-- value(004, 'Anjali', 20, 'A', 94, 'Balasore', 'anjalipanda22@gmail.com');
+
+-- insert into student_table (id, name, age, grade, marks, city,email)
+-- value(005, 'Rimun', 24, 'B', 81, 'Bhadrak', 'rimun24@gmail.com');
+
+-- insert into student_table (id, name, age, grade, marks, city,email)
+-- value(006, 'Rahul', 18, 'O', 98, 'Bhubaneswar', 'rahul18@gmail.com');
+
+-- insert into student_table (id, name, age, grade, marks, city,email)
+-- value(007, 'Abhisekh', 22, 'C', 75, 'Bhubaneswar', 'abhi22@gmail.com');
+
+-- select * from student_table
+
+-- select * from student_table 
+-- where city= 'Bhubaneswar';
+
+-- select * from student_table
+-- where marks > 80;
+
+-- select * from student_table
+-- order by marks desc;
+
+-- SELECT grade, count(*) AS total_students
+-- FROM student_table
+-- GROUP BY grade;
+
+-- select * from student_table
+-- order by age asc
+-- limit 1;
+
+
